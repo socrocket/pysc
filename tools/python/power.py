933922ebad3aa21d26d6829233865aed33690387
Rolf Meyer <meyer@c3e.cs.tu-bs.de>
2014-08-14 10:37:07 +0200

    Tools: Adding Python powermonitor

---
+++ tools/python/power.py
@@ -0,0 +1,66 @@
+import pysc
+import sys
+
+def load(*k, **kw):
+    """Load initial model configuration"""
+    from tools.python.arguments import parser
+    parser.add_argument('-o', '--option', dest='option', action='append', type=str, help='Give configuration option')
+    parser.add_argument('-p', '--python', dest='python', action='append', type=file, help='Execute python scripts')
+    parser.add_argument('-j', '--json', dest='json', action='append', type=str, help='Read JSON configuration')
+    parser.add_argument('-l', '--list', dest='list', action='store_true', help='List options')
+
+def view(*k, **kw):
+    """View detailed model configuration"""
+    total = {}
+    out_category = {
+        "sta_power": "Static power (leakage): %0.4f pW",
+        "int_power": "Internal power (dynamic): %0.4f uW",
+        "swi_power": "Switching power (dynamic): %0.4f uW"
+    }
+    params = pysc.api.gc.readPropertyDict()
+    params = pysc.api.gc.filterDict(params, "power")
+    param_list = pysc.api.gc.paramsToDict(params)
+    out = {}
+    for base, value in param_list.iteritems():
+        parts = base.rsplit(".power.", 1)
+        if len(parts) == 2:
+          name = parts[0]
+          var = parts[1]
+          if not out.has_key(name):
+            out[name] = dict()
+          out[name][var] = value
+        else:
+          print "Mal formated power parameter:", base
+        
+    for comp, var in out.iteritems():
+        print "*****************************************************"
+        print "* Component:", comp
+        print "* ---------------------------------------------------"
+        for name, val in var.iteritems():
+            if name in out_category.keys():
+                print "*", out_category[name] % val
+                if not total.has_key(name):
+                  total[name] = 0.0
+                total[name] += val
+        print "*****************************************************"
+    print "*****************************************************"
+    print "* Power Summary:", comp
+    print "* ---------------------------------------------------"
+    total_sum = 0.0
+    total["sta_power"] /= 10e+6
+    for name in out_category.keys():
+        print "*", out_category[name] % total[name]
+        total_sum += total[name]
+    print "* ---------------------------------------------------"
+    print "* Total Power: %0.4f" % total_sum, "uW"
+    print "*****************************************************"
+    #pysc.api.gc.printDict(params)
+    sys.exit()
+
+def install():
+    load()
+    pysc.on("end_of_evaluation")(view)
+
+if __name__ == "__main__":
+    install()
+
