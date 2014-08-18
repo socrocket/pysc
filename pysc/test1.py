413a67bb7b121d27f161c4f1a99c27081f70cc03
Rolf Meyer <meyer@c3e.cs.tu-bs.de>
2014-08-18 17:32:13 +0200

    Tools: Adding Python Arguments handler

--- pysc/test1.py
+++ pysc/test1.py
@@ -1,22 +1,25 @@
 import pysc
 import json
 
-@pysc.on("end_of_elaboration")
-def do_elab(phase):
-    print "Elab"
+#@pysc.on("end_of_elaboration")
+#def do_elab(phase):
+#    print "Elab"
     #print json.dumps(pysc.gc.readValueDict(), indent=2, sort_keys=True)
     #print pysc.gc.readDict()
 
-def elf_file(name="", value="", time=None, type=None):
-    print "XX", name, value , time, type
+#def elf_file(name="", value="", time=None, type=None):
+#    print "XX", name, value , time, type
 
-@pysc.on("end_of_initialization")
-def do_init(phase):
-    print "Init"
-    pysc.gc.on("ahbctrl.performance_counters.bytes_read", pysc.gc.post_write)(elf_file)
+#@pysc.on("end_of_initialization")
+#def do_init(phase):
+#    print "Init"
+    #pysc.gc.on("ahbctrl.performance_counters.bytes_read", pysc.gc.post_write)(elf_file)
 
 @pysc.on("start_of_initialization")
+@pysc.on("end_of_initialization")
 @pysc.on("start_of_elaboration")
+@pysc.on("end_of_elaboration")
+@pysc.on("start_of_simulation")
 @pysc.on("end_of_simulation")
 @pysc.on("start_of_evaluation")
 @pysc.on("end_of_evaluation")
