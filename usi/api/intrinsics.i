cb6578f64b7679ae72d0ec0f242640378ba1c50a
Rolf Meyer <rolf.meyer@durania.net>
2015-07-15 03:22:00 +0200

    Changeing OSEmulation into Platform intrinsics. This can be used with python to add every function as an intrinsic

---
+++ usi/api/intrinsics.i
@@ -0,0 +1,72 @@
+// vim : set fileencoding=utf-8 expandtab noai ts=4 sw=4 :
+/// @addtogroup pysc
+/// @{
+/// @file intrinsic.i
+/// 
+/// @date 2013-2014
+/// @copyright All rights reserved.
+///            Any reproduction, use, distribution or disclosure of this
+///            program, without the express, prior written consent of the 
+///            authors is strictly prohibited.
+/// @author Rolf Meyer
+%module intrinsics
+
+%include "std_string.i"
+%include "stdint.i"
+%include "usi.i"
+%include "typemaps.i"
+
+USI_REGISTER_MODULE(intrinsics)
+
+%{
+#include "core/common/sr_iss/intrinsics/intrinsicmanager.h"
+#include "core/common/sr_iss/intrinsics/platformintrinsic.h"
+%}
+
+%inline %{
+template<class issueWidth>
+class IntrinsicInterface {
+  public:
+#ifndef SWIG
+    IntrinsicInterface(): m_manager(NULL) {}
+    IntrinsicInterface(IntrinsicManager<issueWidth> *manager): m_manager(manager) {}
+#endif
+    unsigned int get_exit_value() {
+      return m_manager->processorInstance.getExitValue();
+    }
+    bool register_intrinsic(issueWidth addr, sc_core::sc_object *obj) {
+      PlatformIntrinsic<issueWidth> *instance = dynamic_cast<PlatformIntrinsic<issueWidth> *>(obj);
+      if(obj) {
+        m_manager->register_intrinsic(addr, *instance);
+        instance->setManager(m_manager);
+        instance->setProcessor(&m_manager->processorInstance);
+        return true;
+      } else {
+        // TODO(rmeyer): Error
+        return false;
+      }
+    }
+  private:
+    IntrinsicManager<issueWidth> *m_manager;
+};
+%}
+%include "core/common/sr_iss/intrinsics/platformintrinsic.h"
+%template (IntrinsicInterface32) IntrinsicInterface<unsigned int>;
+%template (PlatformIntrinsic32) PlatformIntrinsic<unsigned int>;
+%{
+typedef IntrinsicInterface<unsigned int> IntrinsicInterface32;
+typedef PlatformIntrinsic<unsigned int> PlatformIntrinsic32;
+#define SWIGTYPE_p_IntrinsicInterface32 SWIGTYPE_p_IntrinsicInterfaceT_unsigned_int_t
+PyObject *find_usi_intrinsicmanager32(sc_core::sc_object *obj, std::string name) {
+  IntrinsicManager<unsigned int> *instance = dynamic_cast<IntrinsicManager<unsigned int> *>(obj);
+  if(instance) {
+    return SWIG_NewPointerObj(SWIG_as_voidptr(new IntrinsicInterface32(instance)), SWIGTYPE_p_IntrinsicInterfaceT_unsigned_int_t, SWIG_POINTER_OWN | 0);
+  } else {
+    return NULL;
+  }
+}
+USI_REGISTER_OBJECT_GENERATOR(find_usi_intrinsicmanager32);
+#define SWIGTYPE_p_PlatformIntrinsic32 SWIGTYPE_p_PlatformIntrinsicT_unsigned_int_t
+USI_REGISTER_OBJECT(PlatformIntrinsic32);
+%}
+
--- usi/tools/elf.py
+++ usi/tools/elf.py
@@ -1,39 +1,142 @@
 import usi
 import sys
+import re
 from usi.tools.args import parser, get_args
+from elftools.elf.elffile import ELFFile
+from elftools.elf import constants
+from usi.api import intrinsics
+from usi.api import registry
 
+memoryre = re.compile(r"^(?P<object>[a-zA-Z0-9_.]+)=(?P<filename>[a-zA-Z0-9_./]+)(\((?P<baseaddr>\d+|0x\d+)\))?$", re.U)
+intrinsicre = re.compile(r"^(?P<object>[a-zA-Z0-9_.]+)=(?P<filename>[a-zA-Z0-9_./]+)(\((?P<intrinsics>[a-zA-Z0-9_.,]+)\))?$", re.U)
+intrinsic_groups = {
+    'standard': {
+      '_exit': '_exitIntrinsic32',
+      'chmod': 'chmodIntrinsic32',
+      'chown': 'chownIntrinsic32',
+      'close': 'closeIntrinsic32',
+      'creat': 'creatIntrinsic32',
+      'dup2': 'dup2Intrinsic32',
+      'dub': 'dupIntrinsic32',
+      'error': 'errorIntrinsic32',
+      'fstat': 'fstatIntrinsic32',
+      'getpid': 'getpidIntrinsic32',
+      'gettimeofday': 'gettimeofdayIntrinsic32',
+      'isatty': 'isattyIntrinsic32',
+      'kill': 'killIntrinsic32',
+      'lseek': 'lseekIntrinsic32',
+      'lstat': 'lstatIntrinsic32',
+      'main': 'mainIntrinsic32',
+      'open': 'openIntrinsic32',
+      'random': 'randomIntrinsic32',
+      'read': 'readIntrinsic32',
+      'sbrk': 'sbrkIntrinsic32',
+      'stat': 'statIntrinsic32',
+      'time': 'timeIntrinsic32',
+      'times': 'timesIntrinsic32',
+      'unlink': 'unlinkIntrinsic32',
+      'usleep': 'usleepIntrinsic32',
+      'utimes': 'utimesIntrinsic32',
+      'write': 'writeIntrinsic32'
+    }
+}
 parser.add_argument('-e', '--loadelf', dest='loadelf', action='append', default=[], type=str, help='Load Data from ELF file into memory')
+parser.add_argument('-i', '--intrinsics', dest='intrinsics', action='append', default=[], type=str, help='Load intrinsics for an elf file to a processor')
+
+def load_elf_into_scireg(filename, stores, base):
+    with open(filename, "rb") as stream:
+        elf = ELFFile(stream)
+        for section in elf.iter_sections():
+            if section.header["sh_flags"] & constants.SH_FLAGS.SHF_ALLOC:
+                addr = section.header["sh_addr"] - base
+                data = section.data()
+                
+                if isinstance(stores, usi.USIDelegate):
+                    stores = [stores]
+                for store in stores:
+                    print "Loading %s into %s at address %s" % (filename, store.name(), base)
+                    store.scireg_write(data, addr)
+
+def load_elf_intrinsics_to_processor(filename, cpus, intrinsics):
+    with open(filename, "rb") as stream:
+        elf = ELFFile(stream)
+        for section in elf.iter_sections():
+            if section.header['sh_type'] == 'SHT_SYMTAB':
+                for name, klass, entry in [(symbol.name, intrinsics[symbol.name], symbol.entry) for symbol in section.iter_symbols() if symbol.name in intrinsics.keys()]:
+                    for cpu in cpus:
+                        intrinsic_manager = None
+                        if 'register_intrinsic' in dir(cpu):
+                            intrinsic_manager = cpu
+                        else:
+                            for child in cpu.children():
+                                if child.basename() == 'intrinsics' and 'register_intrinsic' in dir(child):
+                                    intrinsic_manager = child
+                                    break
+
+                        if not intrinsic_manager:
+                            print "intrinsic manager for cpu %s not found" % cpu.name()
+                        print "Intrinsic on symbol %s at address %x is inserted with class %s " % (name, entry['st_value'], klass)
+                        intrinsic_instance = registry.create_object_by_name('PlatformIntrinsic', klass, name)
+                        intrinsic_manager.register_intrinsic(entry['st_value'], intrinsic_instance)
 
 @usi.on('start_of_simulation')
 def start_of_initialization(*k, **kw):
-    from elftools.elf.elffile import ELFFile
-    from elftools.elf import constants
     for param in get_args().loadelf:
-        paramlist = param.split('=')
-        if len(paramlist) != 2:
+        result = memoryre.match(param)
+        if not result:
             print "-e takes always a key/value pair. '%s' is not a key/value pair. The value must be contain a file name and a base address: '-e sdram=hello.sparc(0x40000000)'" % (param)
-        obj = paramlist[0]
-        paramlist2 = paramlist[1].split('(')
-        if len(paramlist2) > 2:
+            continue
+        groups = result.groupdict()
+        if not 'object' in groups or not 'filename' in groups:
             print "-e takes always a key/value pair. '%s' is not a key/value pair. The value must be contain a file name and a base address: '-e sdram=hello.sparc(0x40000000)'" % (param)
-        filename = paramlist2[0]
-        if len(paramlist2) == 2:
-            base = int(paramlist2[1].strip(')'), 0) # not 100% right, won't work in the case when there is no 0x in front of Hex string!
-        elif filename.endswith('.sparc'):
+            continue
+        obj = groups['object']
+        filename = groups['filename']
+        if "baseaddr" in groups and groups['baseaddr']:
+            print groups['baseaddr']
+            base = int(groups['baseaddr'], 0) # not 100% right, won't work in the case when there is no 0x in front of Hex string!
+        elif filename.endswith('.sparc') or filename.endswith('.dsu'):
             base = 0x40000000
         else:
             base = 0x00000000
         stores = usi.find(obj)
         if len(stores) == 0:
             print "scireg %s not found in simulation for parameter -e %s" % (obj, param)
+            continue
 
-        print "Loading %s into %s at address %s" % (filename, obj, base)
-        with open(filename, "rb") as stream:
-            elf = ELFFile(stream)
-            for section in elf.iter_sections():
-                if section.header["sh_flags"] & constants.SH_FLAGS.SHF_ALLOC:
-                    addr = section.header["sh_addr"] - base
-                    data = section.data()
-                    
-                    for store in stores:
-                        store.scireg_write(data, addr)
+        load_elf_into_scireg(filename, stores, base)
+
+    for param in get_args().intrinsics:
+        result = intrinsicre.match(param)
+        if not result:
+            print "-i takes always a key/value pair. '%s' is not a key/value pair. The value must be contain a file name and a list of intrinsics: '-i leon3_0=hello.sparc(open,close)'" % (param)
+            continue
+        groups = result.groupdict()
+        if not 'object' in groups or not 'filename' in groups or not 'intrinsics':
+            print "-e takes always a key/value pair. '%s' is not a key/value pair. The value must be contain a file name and a base address: '-i leon3_0=hello.sparc(open,close)'" % (param)
+            continue
+        obj = groups['object']
+        filename = groups['filename']
+        intrinsiclist = groups['intrinsics'].split(',')
+        intrinsics = {}
+        for intrinsic in intrinsiclist:
+            splitted = intrinsic.split('=')
+            if len(splitted) == 1:
+                if splitted[0] in intrinsic_groups:
+                    intrinsics.update(intrinsic_groups[splitted[0]])
+                else:
+                    intrinsics[splitted[0]] = splitted[0]
+                
+            elif len(splitted) > 1:
+                if splitted[1] == 'None':
+                    del(intrinsics[splitted[0]])
+                else:
+                    intrinsics[splitted[0]] = splitted[1]
+            else:
+                pass
+        cpus = usi.find(obj)
+        if len(cpus) == 0:
+            print "cpu %s not found in simulation for parameter -i %s" % (obj, param)
+            continue
+
+        load_elf_intrinsics_to_processor(filename, cpus, intrinsics)
