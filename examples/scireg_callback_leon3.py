c3584944eff3facabc1e60f383857be1cc065409
Jan Wagner <jaydyou@janidom.de>
2016-03-02 18:33:04 +0100

    improved leon3 callback example

--- examples/scireg_callback_leon3.py
+++ examples/scireg_callback_leon3.py
@@ -6,10 +6,9 @@ from sr_register import scireg
 def simulation_begin(*k, **kw):
     reg0 = usi.USIDelegate('leon3_0.leon3.PC')
     def foo(*k, **kw):
-        print "Yay"
-        #print("Callback: Access at address {} with length of {} bytes on {}."\
-        #        .format(k[0], k[1], k[2].scireg_get_string_attribute(scireg.SCIREG_NAME)))
+        print("Callback: Access at address {} with length of {} bytes on {}."\
+                .format(k[0], k[1], k[2].scireg_get_string_attribute(scireg.SCIREG_NAME)))
     reg0.scireg_add_callback((foo, scireg.SCIREG_READ_ACCESS, long(0), long(4)))
-    print('PC value: {}'.format(reg0.scireg_read(long(4))))
+    #print('PC value: {}'.format(reg0.scireg_read(long(4))))
     print('end of callback example')
     #sys.exit()
